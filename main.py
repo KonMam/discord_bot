74fda708b195ac46525984e6f5326b02d4fc2826
KonMam <k.mamonas@gmail.com>
2022-12-11 00:08:23 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+
+import os
+import dotenv
+import discord
+from discord.ext import commands
+
+dotenv.load_dotenv()
+
+TOKEN = os.getenv('APP_TOKEN')
+print(TOKEN)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.command(name='hello')
+async def hello(ctx):
+    await ctx.send('Hello World!')
+
+bot.run(token=TOKEN)
